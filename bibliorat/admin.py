85ce24bd60c0bbcb0cf9fa97cd98b2c1aeb5a81a
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-09-30 16:00:34 +0000

    Add second model to bibliorat/admin.py

--- bibliorat/admin.py
+++ bibliorat/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Bookprofile
-from .models import Bookauthor
+from .models import Bookprofile, Bookauthor
 
 # Register your models here.
 admin.site.register(Bookprofile)
