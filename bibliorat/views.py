c6eeec9283ee228e3a66a0b828b848fc7ae31437
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-10-03 20:45:09 +0000

    Style pagination as function of current page and total number of pages in bibliorat

--- bibliorat/views.py
+++ bibliorat/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views import generic
 from .models import Bookprofile
 
 # Create your views here.
 
 class BookprofileList(generic.ListView):
-    queryset = Bookprofile.objects.filter(status=1).order_by('?')
+    queryset = Bookprofile.objects.filter(status=1).order_by('-created_on')
     template_name = 'bibliorat/index.html'
-    # context_object_name = 'bookprofiles'
     paginate_by = 6
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+
+        # Calculate the second to last page
+        paginator = context['paginator']
+        if paginator.num_pages > 1:
+            context['second_to_last_page'] = paginator.num_pages - 1
+        else:
+            context['second_to_last_page'] = None  # No second to last page available
+
+        return context
