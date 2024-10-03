0d7a917f545293145d9438358b10e060d931b4e7
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-10-03 16:42:44 +0000

    Add Summernote package and set up the admin page accordingly

--- bibliorat/admin.py
+++ bibliorat/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Bookauthor, Bookprofile, Bookreview
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Bookprofile)
+class BookprofileAdmin(SummernoteModelAdmin):
+
+    list_display = ('booktitle', 'slug', 'authorname', 'status', 'profileauthor', 'publicationyear', 'originallanguage', 'bookgenre')
+    search_fields = ['booktitle']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('booktitle',)}
+    summernote_fields = ('profilesynopsis', 'profileanalysis',)
 
 # Register your models here.
 admin.site.register(Bookauthor)
-admin.site.register(Bookprofile)
+# admin.site.register(Bookprofile)
 admin.site.register(Bookreview)
--- bookworm/urls.py
+++ bookworm/urls.py
@@ -18,7 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("bibliorat.urls"), name="bibliorat-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("bibliorat.urls"), name="bibliorat-urls"),
 ]
 
+# order paths alphabetically, empty ones last
