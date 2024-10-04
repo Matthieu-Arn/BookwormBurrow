e2e70a2993fe31fbc78d4b0bc554871216d07d47
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-10-04 03:17:39 +0000

    Add label to bookreview form

--- bibliorat/forms.py
+++ bibliorat/forms.py
@@ -6,3 +6,6 @@ class BookreviewForm(forms.ModelForm):
     class Meta:
         model = Bookreview
         fields = ('reviewcontent',)
+        labels = {
+            'reviewcontent': 'Your Review ',  # Change this text
+        }
