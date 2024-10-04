3e53beb20d487ad1157d8f61cd29057e710432b1
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-10-04 10:16:11 +0000

    Create structure for wishlist view

--- bookwish/views.py
+++ bookwish/views.py
@@ -3,7 +3,15 @@ from .models import Bookwishitem
 from bibliorat.models import Bookprofile
 
 # Create your views here.
-def mybookwishitems(request, slug):
+def mybookwishitems(request):
+    if request.user.is_authenticated:
+        bookprofile = get_object_or_404(Bookprofile, slug=slug)
+        # Create a new wishlist item
+        Bookwishitem.objects.get_or_create(listowner=request.user, booktitle=bookprofile)
+    return redirect('bookprofile_detail', slug=slug)
+
+
+def reading_list(request): 
     """
     Display an individual :model:`bookwish.Bookwishitem`.
     Renders the Bookwishitem page
@@ -14,10 +22,9 @@ def mybookwishitems(request, slug):
     :template:`bookwish/bookwish.html`
     """
     if request.user.is_authenticated:
-        bookprofile = get_object_or_404(Bookprofile, slug=slug)
-        # Create a new wishlist item
-        Bookwishitem.objects.get_or_create(listowner=request.user, booktitle=bookprofile)
-    return redirect('bookprofile_detail', slug=slug)
+        reading_list_items = Bookwishitem.objects.filter(listowner=request.user).select_related('booktitle')
+        return render(request, 'bookwish/reading_list.html', {'reading_list_items': reading_list_items})
+    return redirect('login')  # Redirect to login if not authenticated
 
 
 def add_to_reading_list(request, slug):
