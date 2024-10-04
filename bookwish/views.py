41ff5bfed561f48e6faee810f5690aa5bcc6ba13
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-10-04 00:39:00 +0000

    Create view for bookwish

--- bookwish/views.py
+++ bookwish/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Bookwishitem
+
 
 # Create your views here.
+def mybookwishitems(request):
+    """
+    Display an individual :model:`bookwish.Bookwishitem`.
+    Renders the Bookwishitem page
+
+    **Context**
+
+    ``bookwishitem``
+        An instance of :model:`bookwish.Bookwishitem`.
+
+    **Template:**
+
+    :template:`bookwish/bookwish.html`
+    """
+    
+    bookwish = Bookwishitem.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "bookwish/bookwish.html",
+        {"bookwish": bookwish},
+    )
+
--- bookworm/urls.py
+++ bookworm/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("bookwish/", include("bookwish.urls"), name="bookwish-urls"),
     path('summernote/', include('django_summernote.urls')),
     path("", include("bibliorat.urls"), name="bibliorat-urls"),
 ]
