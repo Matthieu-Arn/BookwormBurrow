37e91513bc1fff8b2fa909a0c8721ef87c96de8b
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-10-04 01:25:56 +0000

    Add AllAuth package

--- bookworm/urls.py
+++ bookworm/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path("bookwish/", include("bookwish.urls"), name="bookwish-urls"),
     path('summernote/', include('django_summernote.urls')),
