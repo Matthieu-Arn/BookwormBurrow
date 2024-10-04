fd2b18ea102d9e83363202d81904aea7e543811f
Mathieu Arnoult <ubwwn18o@students.codeinstitute.net>
2024-10-04 09:46:45 +0000

    Create views for adding/deleting items from the wish list

--- bookwish/views.py
+++ bookwish/views.py
@@ -1,28 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Bookwishitem
-
+from bibliorat.models import Bookprofile
 
 # Create your views here.
-def mybookwishitems(request):
-    """
-    Display an individual :model:`bookwish.Bookwishitem`.
-    Renders the Bookwishitem page
 
-    **Context**
 
-    ``bookwishitem``
-        An instance of :model:`bookwish.Bookwishitem`.
+def add_to_reading_list(request, slug):
+    # Add item to wishlist
+    if request.user.is_authenticated:
+        bookprofile = get_object_or_404(Bookprofile, slug=slug)
+        Bookwishitem.objects.get_or_create(listowner=request.user, booktitle=bookprofile)
+    return redirect('bookprofile_detail', slug=slug)
 
-    **Template:**
-
-    :template:`bookwish/bookwish.html`
-    """
-    
-    bookwish = Bookwishitem.objects.all().order_by('-updated_on').first()
-
-    return render(
-        request,
-        "bookwish/bookwish.html",
-        {"bookwish": bookwish},
-    )
-
+def remove_from_reading_list(request, slug):
+    # Remove item from wishlist
+    if request.user.is_authenticated:
+        bookprofile = get_object_or_404(Bookprofile, slug=slug)
+        Bookwishitem.objects.filter(listowner=request.user, booktitle=bookprofile).delete()
+    return redirect('bookprofile_detail', slug=slug)
